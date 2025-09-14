008d453f0f18ed4a641f7b4eb59ead38eae8ca2c
mndatko <136758450+mndatko@users.noreply.github.com>
2025-09-14 17:59:41 +0200

    Update partials-company-atao.js

--- partials-company-atao.js
+++ partials-company-atao.js
@@ -1,4 +1,4 @@
-<script>
+
     if(signUpHint === "ATAO") {
         const nextButton = document.getElementById('step1-nextButton-atao');
         const step1 = document.getElementById('step1-atao');
@@ -85,4 +85,3 @@
 
         });
     }
-</script>
